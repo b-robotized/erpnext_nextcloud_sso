80bd8654149a7d164dd51e6a05a728254630edd6
Dr. Denis <denis.stogl@b-robotized.com>
2026-02-14 02:24:56 +0100

    Add Nextcloud as Social Login Provider

---
+++ erpnext_nextcloud_sso/install.py
@@ -0,0 +1,37 @@
+import frappe
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+
+from .property_setters import get_property_setters
+
+
+def after_install():
+	"""
+	Called after the app is installed on a site.
+	
+	This adds "Nextcloud" to the Social Login Provider dropdown.
+	"""
+	make_property_setters()
+
+
+def make_property_setters():
+	"""
+	Apply property setters to extend DocType fields.
+	"""
+	for doctypes, property_setters in get_property_setters().items():
+		if isinstance(doctypes, str):
+			doctypes = (doctypes,)
+
+		for doctype in doctypes:
+			for property_setter in property_setters:
+				fieldname = property_setter[0]
+				property_name = property_setter[1]
+				value = property_setter[2]
+
+				# Property type is determined automatically by make_property_setter
+				make_property_setter(
+					doctype=doctype,
+					fieldname=fieldname,
+					property=property_name,
+					value=value,
+					property_type="Text",  # Options field is text type
+				)
