a48d105ba6dd9b3e7577022aac8c1cc5f8dbe48e
arungovind1989 <arun.govind@cloudconverge.io>
2026-03-21 02:24:32 +0530

    Remove Nextcloud before uninstallation (#3)
    ---------
    
    Co-authored-by: Dr. Denis <denis.stogl@b-robotized.com>

--- erpnext_nextcloud_sso/setup.py
+++ erpnext_nextcloud_sso/setup.py
@@ -20,3 +20,25 @@ def add_nextcloud_provider():
         )
 
         frappe.clear_cache()
+
+
+def remove_nextcloud_provider():
+    df = frappe.get_meta("Social Login Key").get_field("social_login_provider")
+
+    if not df:
+        return
+
+    options = df.options.split("\n") if df.options else []
+    if "Nextcloud" not in options:
+        return
+
+    options = [option for option in options if option != "Nextcloud"]
+
+    frappe.db.set_value(
+        "DocField",
+        {"parent": "Social Login Key", "fieldname": "social_login_provider"},
+        "options",
+        "\n".join(options),
+    )
+
+    frappe.clear_cache()
